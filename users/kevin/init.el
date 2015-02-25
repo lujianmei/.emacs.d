2ff70c7ed1ca19b2d1af912905a846bb77d884be
lujianmei <anysky130@163.com>
2015-02-25 14:55:57 +0000

    add flymake for css/js/sass, add sass-mode, add some shot-key for indent-region when editing or selected text.

--- users/kevin/init.el
+++ users/kevin/init.el
@@ -3,6 +3,30 @@
 (add-hook 'sgml-mode-hook 'zencoding-mode) ;; Auto-start on any markup modes
 
 
+
+;;add flymake-easy which is the base function for flymake series.
+(require 'flymake-easy)
+(add-hook 'js-mode-hook 'flymake-easy-load)
+
+;;add flymake-jslint which is for syntax-checking Javascript source code using jslint, install jslint first, check on:install note.js, then execute: npm -g install jslint to install jslint
+(require 'flymake-jslint)
+(add-hook 'js-mode-hook 'flymake-jslint-load)
+
+;;add flymake-css wihch An Emacs flymake handler for syntax-checking CSS source code using csslint(https://github.com/CSSLint/csslint).
+(require 'flymake-css)
+(add-hook 'css-mode-hook 'flymake-css-load)
+
+;;add flymake-sass for checking sass files.
+(require 'flymake-sass)
+(add-hook 'sass-mode-hook 'flymake-sass-load)   
+
+
+;; Fixed the indentation of line just after the style tag.
+(autoload 'css-mode "css-mode")
+(setq auto-mode-alist
+        (cons '("\\.css\\'" . css-mode) auto-mode-alist))
+
+
 ;;设置窗口位置为屏库左上角(0,0)
 (set-frame-position (selected-frame) 6 0)
 ;;设置宽和高,大家可以调整这个参数来适应自己屏幕大小，下面的数字是显示字符数量
@@ -10,3 +34,19 @@
 (add-to-list 'default-frame-alist '(height . 43))
 
 (split-window-right 50)
+
+
+
+;; tab 缩进
+(setq c-basic-offset 4)
+(setq indent-tabs-mode nil)
+(setq default-tab-width 4)
+(setq tab-width 4)
+(setq tab-stop-list ())
+(loop for x downfrom 40 to 1 do
+(setq tab-stop-list (cons (* x 4) tab-stop-list)))
+ 
+;; 回车缩进
+(global-set-key "\C-m" 'reindent-then-newline-and-indent)
+(global-set-key (kbd "C-<return>") 'reindent-then-newline-and-indent)
+(global-set-key (kbd "C-S-i") 'indent-region)
